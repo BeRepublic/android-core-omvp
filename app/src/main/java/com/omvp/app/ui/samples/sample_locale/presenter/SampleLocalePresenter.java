509184dd723d40ef11ae0fad3bde8b0d76c4e02c
Ángel Gómez <angel.gomez@berepublic.es>
2018-03-26 18:11:41 +0200

    locale implemented

--- app/src/main/java/com/omvp/app/ui/samples/sample_locale/presenter/SampleLocalePresenter.java
+++ app/src/main/java/com/omvp/app/ui/samples/sample_locale/presenter/SampleLocalePresenter.java
@@ -1,4 +1,4 @@
-package com.omvp.app.ui.samples.sample.presenter;
+package com.omvp.app.ui.samples.sample_locale.presenter;
 
 import com.omvp.app.base.mvp.presenter.Presenter;
 
@@ -6,6 +6,10 @@ import com.omvp.app.base.mvp.presenter.Presenter;
  * Created by Angel on 21/02/2018.
  */
 
-public interface SamplePresenter extends Presenter {
+public interface SampleLocalePresenter extends Presenter {
+
+    void localeSelected(int position);
+
+    void changeLocale();
 
 }
--- app/src/main/java/com/omvp/app/ui/samples/sample_locale/presenter/SampleLocalePresenterImpl.java
+++ app/src/main/java/com/omvp/app/ui/samples/sample_locale/presenter/SampleLocalePresenterImpl.java
@@ -1,61 +1,65 @@
 
-package com.omvp.app.ui.samples.sample.presenter;
+package com.omvp.app.ui.samples.sample_locale.presenter;
 
 
 import com.omvp.app.base.mvp.presenter.BasePresenter;
+import com.omvp.app.base.reactivex.BaseDisposableCompletableObserver;
 import com.omvp.app.base.reactivex.BaseDisposableSingleObserver;
-import com.omvp.app.model.mapper.SampleModelDataMapper;
-import com.omvp.app.ui.samples.sample.view.SampleView;
-import com.omvp.domain.SampleDomain;
-import com.omvp.domain.interactor.GetSampleListUseCase;
-import com.omvp.domain.interactor.GetSampleUseCase;
+import com.omvp.app.ui.samples.sample_locale.view.SampleLocaleView;
+import com.omvp.app.util.LocaleHelper;
+import com.omvp.domain.interactor.GetLocaleListUseCase;
+import com.omvp.domain.interactor.GetLocaleUseCase;
+import com.omvp.domain.interactor.SaveLocaleUseCase;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableMaybeObserver;
-import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
-public class SamplePresenterImpl extends BasePresenter<SampleView> implements SamplePresenter {
+public class SampleLocalePresenterImpl extends BasePresenter<SampleLocaleView> implements SampleLocalePresenter {
 
     @Inject
-    GetSampleUseCase mGetSampleUseCase;
+    GetLocaleUseCase mGetLocaleUseCase;
     @Inject
-    GetSampleListUseCase mGetSampleListUseCase;
+    GetLocaleListUseCase mGetLocaleListUseCase;
     @Inject
-    GetSampleListUseCase mGetSampleListUseCase2;
-    @Inject
-    SampleModelDataMapper mSampleModelDataMapper;
+    SaveLocaleUseCase mSaveLocaleUseCase;
 
-    private long mSampleId;
+    private Locale mLocale;
+    private List<Locale> mLocaleList;
 
     @Inject
-    public SamplePresenterImpl(SampleView sampleView) {
-        super(sampleView);
+    public SampleLocalePresenterImpl(SampleLocaleView sampleLocaleView) {
+        super(sampleLocaleView);
     }
 
     @Override
     public void onViewLoaded() {
         super.onViewLoaded();
 
-        loadSample();
+        loadLocale();
     }
 
-    public void setSampleId(long id) {
-        mSampleId = id;
-    }
-
-    private void loadSample() {
-        mDisposableManager.add(mGetSampleUseCase.execute(mSampleId)
+    @Override
+    public void changeLocale() {
+        mDisposableManager.add(mGetLocaleListUseCase.execute()
+                .map(new Function<List<Locale>, List<Locale>>() {
+                    @Override
+                    public List<Locale> apply(List<Locale> localeList) throws Exception {
+                        mLocaleList = localeList;
+                        return localeList;
+                    }
+                })
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
-                .subscribeWith(new BaseDisposableSingleObserver<SampleDomain>(mContext) {
+                .subscribeWith(new BaseDisposableSingleObserver<List<Locale>>(mContext) {
                     @Override
                     protected void onError(int code, String title, String description) {
-                        hideProgress();
                         showError(code, title, description);
                     }
 
@@ -65,17 +69,81 @@ public class SamplePresenterImpl extends BasePresenter<SampleView> implements Sa
                     }
 
                     @Override
-                    public void onSuccess(SampleDomain sampleDomain) {
-                        hideProgress();
-                        drawSampleItem(sampleDomain);
+                    public void onSuccess(List<Locale> localeList) {
+                        drawLocaleSelector(localeList);
                     }
                 }));
     }
 
-    private void drawSampleItem(SampleDomain sampleDomain) {
+    @Override
+    public void localeSelected(int position) {
+        final Locale locale = mLocaleList.get(position);
+        mDisposableManager.add(mSaveLocaleUseCase.execute(locale)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribeWith(new BaseDisposableCompletableObserver(mContext) {
+                    @Override
+                    protected void onError(int code, String title, String description) {
+                        showError(code, title, description);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        LocaleHelper.updateConfiguration(mResources, locale);
+                        drawLocale(locale);
+                    }
+                }));
+    }
+
+    private void loadLocale() {
+        mDisposableManager.add(mGetLocaleUseCase.execute()
+                .map(new Function<Locale, Locale>() {
+                    @Override
+                    public Locale apply(Locale locale) throws Exception {
+                        mLocale = locale;
+                        return locale;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribeWith(new BaseDisposableSingleObserver<Locale>(mContext) {
+                    @Override
+                    protected void onError(int code, String title, String description) {
+                        showError(code, title, description);
+                    }
+
+                    @Override
+                    protected void onStart() {
+                        showProgress();
+                    }
+
+                    @Override
+                    public void onSuccess(Locale locale) {
+                        drawLocale(locale);
+                    }
+                }));
+    }
+
+    private void drawLocale(Locale locale) {
         if (mView != null) {
-            mView.drawTitle(sampleDomain.getTitle());
-            mView.drawImage(sampleDomain.getImageResId());
+            mView.drawLocale(locale.toString());
         }
     }
+
+    private void drawLocaleSelector(List<Locale> localeList) {
+        if (mView != null) {
+            int selection = -1;
+            int count = 0;
+            List<String> localeStringList = new ArrayList<>();
+            for (Locale locale : localeList) {
+                if (mLocale.getLanguage().equals(locale.getLanguage())) {
+                    selection = count;
+                }
+                localeStringList.add(locale.toString());
+                count ++;
+            }
+            mView.drawLocaleSelector(localeStringList, selection);
+        }
+    }
+
 }
