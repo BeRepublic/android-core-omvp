db72c55f5fbfe93756cdb57078830e3e26703bb5
Stefano Fattorusso <stefano.fattorusso@berepublic.es>
2018-04-05 17:45:00 +0200

    Fix problems after merge

--- app/src/main/java/com/omvp/app/ui/home/HomeActivity.java
+++ app/src/main/java/com/omvp/app/ui/home/HomeActivity.java
@@ -133,5 +133,5 @@ public class HomeActivity extends BaseFragmentActivity implements
     public void onBottomNavigationViewSelected() {
         mNavigationHelper.launchBottomBarSample();
     }
-}
+
 }
